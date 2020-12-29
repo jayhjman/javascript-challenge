87a711a557f04bd29b52ec8cc5cf82494b195801
Jay Hastings <jayhjman@gmail.com>
2020-12-28 18:12:52 -0800

    added some console logging

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,3 +1,15 @@
-#
-#  UFO Application Code
-#
+//
+//   UFO Application Code
+//
+
+var ufos = data;
+
+ufos.forEach((ufo) => {
+      console.log(ufo.datetime);
+      console.log(ufo.city);
+      console.log(ufo.state);
+      console.log(ufo.country);
+      console.log(ufo.shape);
+      console.log(ufo.durationMinutes);
+      console.log(ufo.comments);
+});
