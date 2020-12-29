4d87a1ce0d4f3bb20fd7d6959df24ec7b37f53de
Jay Hastings <jayhjman@gmail.com>
2020-12-28 18:23:57 -0800

    display ufo data in the table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,14 +2,36 @@
 //   UFO Application Code
 //
 
+// Rename data to something more human readable 
 var ufos = data;
 
+// grab a reference to the table body we want to change
+var tbody = d3.select("tbody");
+
+// Do the initial display of all UFO data
 ufos.forEach((ufo) => {
-      console.log(ufo.datetime);
-      console.log(ufo.city);
-      console.log(ufo.state);
-      console.log(ufo.country);
-      console.log(ufo.shape);
-      console.log(ufo.durationMinutes);
-      console.log(ufo.comments);
+      
+    var row = tbody.append("tr");
+
+    var cell = row.append("td")
+    cell.text(ufo.datetime)
+
+    cell = row.append("td")
+    cell.text(ufo.city)
+
+    cell = row.append("td")
+    cell.text(ufo.state)
+
+    cell = row.append("td")
+    cell.text(ufo.country)
+
+    cell = row.append("td")
+    cell.text(ufo.shape)
+
+    cell = row.append("td")
+    cell.text(ufo.durationMinutes)
+
+    cell = row.append("td")
+    cell.text(ufo.comments)
+
 });
